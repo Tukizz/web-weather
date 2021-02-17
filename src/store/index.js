0221ca117223170117dab5b61fb2e1395659d591
tukizz <greatofjar@gmail.com>
2021-02-17 15:56:16 +0700

    fix default city & add day method

--- src/store/index.js
+++ src/store/index.js
@@ -6,9 +6,12 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    city: "Bandung",
+    city: localStorage.getItem("city")
+      ? localStorage.getItem("city")
+      : "Bandung",
     weather: {},
-    location: {}
+    location: {},
+    day: []
   },
   getters: {
     getCity: state => {
@@ -19,33 +22,39 @@ export default new Vuex.Store({
     },
     getLocation: state => {
       return state.location;
+    },
+    getDay: state => {
+      return state.day;
     }
   },
   mutations: {
     selectCity(state, data) {
       state.city = data;
+      localStorage.setItem("city", data);
     },
     pushData(state, data) {
       state.weather = data.weather;
       state.location = data.location;
+      state.day = data.day;
     }
   },
   actions: {
     weatherLoad({ commit, state }) {
       return new Promise((resolve, reject) => {
         axios
-          .get(process.env.VUE_APP_WEATHER_API + "current.json", {
+          .get(process.env.VUE_APP_WEATHER_API + "forecast.json", {
             params: {
               key: process.env.VUE_APP_WEATHER_KEY,
-              q: state.city
+              q: state.city,
+              days: 7
             }
           })
           .then(res => {
             let location = res.data.location;
             let weather = res.data.current;
-
+            let day = res.data.forecast.forecastday;
             commit("selectCity", state.city);
-            commit("pushData", { location, weather });
+            commit("pushData", { location, weather, day });
             resolve(res);
           })
           .catch(err => {
